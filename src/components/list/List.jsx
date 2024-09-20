21de9336a92d6ab99d34599d75b321bcb77b2960
Parth Bedarkar <code63genos@gmail.com>
2024-09-20 22:49:18 +0530

    fix: type error

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,13 @@
 import ChatList from "./chatList/ChatList"
 import "./list.css"
 import GroupChatButton from "./userInfo/GroupChat"
-import Userinfo from "./userInfo/Userinfo"
+import Userinfo from "./userInfo/UserInfo"
 
 const List = () => {
   return (
     <div className='list'>
       <Userinfo/>
       <ChatList/>
-
     </div>
   )
 }
